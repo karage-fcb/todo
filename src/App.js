c8d7e9cb2f4b9642632e2c7e11f5bdf7665fcfa2
yuha-tsuji <yuga7517@gmail.com>
2021-08-12 10:41:35 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css'; 
+import DrfApi from './components/DrfApi';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo APP</h1>
+      <DrfApi />
+    </div>
+  );
+}
+
+export default App;
